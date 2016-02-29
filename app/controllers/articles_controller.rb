b93a2e455a6aa0d4a7fbec842591ea799b80113f
Josh <Tattsy@hotmail.co.uk>
2016-02-29 16:53:30 +0000

    Enable articles to be rendered in markdown
    Articles are now rendered in markdown to adhere to the spec, articles
    can also be created by the signed in blogger.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,19 @@
 class ArticlesController < ApplicationController
+
+  before_action :authenticate_user!, :except => [:index]
+  
   def index
+    @article = Article.new
     @articles = Article.all.reverse
   end
+
+  def create
+    article = Article.create(article_params)
+    redirect_to articles_path
+  end
+
+  def article_params
+    params.require(:article).permit(:content)
+  end
+
 end
--- spec/features/articles_feature_spec.rb
+++ spec/features/articles_feature_spec.rb
@@ -15,4 +15,18 @@ feature "blog" do
       expect(page).not_to have_content "No blog posts have been made."
     end
   end
+  context "when signed in as a blogger" do
+    before(:each) do
+      Blogger.create(email: "example@email.co.uk", password: "randomletters")
+      visit "/"
+      fill_in "blogger_email", with: "example@email.co.uk"
+      fill_in "blogger_password", with: "randomletters"
+      click_button "Sign in"
+    end
+    scenario "articles can be written" do
+      fill_in "article_content", with: "Hello World!!"
+      click_button "Post Article"
+      expect(page).to have_content "Hello World!!"
+    end
+  end
 end
