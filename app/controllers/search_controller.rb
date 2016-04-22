b1eaf280eb71ce8854408ad8828444c12c79cf1b
Josh Tatterton <joshua.tatterton@matalandirect.com>
2016-04-22 17:02:23 +0100

    Establish "strict" search method
    Searches can now be performed, the standard criteria is that all words
    in the search term are located inside of a article title/content to be
    valid.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,40 @@
 class SearchController < ApplicationController
 
   def index
+    @results = search(params[:search])
   end
-  
+
+  private
+
+  def search(term)
+    articles = Article.all
+    terms = term.downcase.split(" ")
+    puts terms
+
+    articles.inject([]) do |memo, article|
+      result = true
+      if article.wysiwyg?
+        content = article.wysiwyg_content.downcase.gsub(" ", "")
+      else
+        content = article.content.downcase.gsub(" ", "")
+      end
+      title = article.title.downcase.gsub(" ", "")
+      puts content
+      puts title
+      terms.each do |t|
+        if content.include?(t) || title.include?(t)
+          result = true unless result == false
+        else
+          result = false
+        end
+      end
+
+      if result
+        memo << article 
+      else
+        memo
+      end
+    end
+  end
+
 end
--- spec/features/search_feature_spec.rb
+++ spec/features/search_feature_spec.rb
@@ -1,12 +1,56 @@
 feature "search" do
   
+  before(:each) { visit "/" }
   scenario "there is a search bar" do
-    visit "/"
-
     fill_in "search", with: "search term"
     click_button "search_button"
 
     expect(current_path).to eq "/search"
+  end
+
+  scenario "user can perform a basic 'strict' search" do
+    blogger = Blogger.create( username: "Joshy", 
+                              email: "example@email.co.uk", 
+                              password: "randomletters", 
+                              password_confirmation: "randomletters")
+    article = blogger.articles.create(title: "My Article", content: "This is some content")
+
+    fill_in "search", with: "content"
+    click_button "search_button"
+
+    expect(page).to have_content blogger.username
+    expect(page).to have_content article.title
+    expect(page).to have_content article.content
+  end
+
+  scenario "searches are not case sensitive" do
+    blogger = Blogger.create( username: "Joshy", 
+                              email: "example@email.co.uk", 
+                              password: "randomletters", 
+                              password_confirmation: "randomletters")
+    article = blogger.articles.create(title: "My Article", content: "This is some content")
+
+    fill_in "search", with: "article"
+    click_button "search_button"
+
+    expect(page).to have_content blogger.username
+    expect(page).to have_content article.title
+    expect(page).to have_content article.content
+  end
+
+  scenario "searches work for wysiwyg articles" do
+    blogger = Blogger.create( username: "Joshy", 
+                              email: "example@email.co.uk", 
+                              password: "randomletters", 
+                              password_confirmation: "randomletters")
+    article = blogger.articles.create(title: "My Article", option: "wysiwyg", wysiwyg_content: "<p>This is some content</p>")
+
+    fill_in "search", with: "this"
+    click_button "search_button"
+
+    expect(page).to have_content blogger.username
+    expect(page).to have_content article.title
+    expect(page).to have_content "This is some content"
   end
 
 end
