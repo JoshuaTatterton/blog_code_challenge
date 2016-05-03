b6c72c74ec918d7df5c767730af0f9234347452c
Josh Tatterton <joshua.tatterton@matalandirect.com>
2016-05-03 12:35:29 +0100

    Refactor adding new article
    The process of adding a new article has been refactored to not use
    JavaScript and instead uses CSS tricks instead. The uneeded JavaScript
    has also been removed.

--- app/assets/javascripts/sitewide/articles.js
+++ app/assets/javascripts/sitewide/articles.js
@@ -1,37 +1,12 @@
 var ready = function() {
 
-  $(".article_writer").hide();
   $(".article_editor").hide();
-  $("#markdown_content_container").hide();
-  $("#wysiwig_content_container").hide();
-
-  $("#new_article").click(function() {
-    if ($(".article_writer").css("display") == "none") {
-      $(".article_writer").show();
-    } else {
-      $(".article_writer").hide();
-    }
-  });
 
   $(".edit_button").click(function() {
     var id = $(this).attr("id");
 
     $("#article_editor_" + id).show();
     $("#article_display_" + id).hide();
-  });
-
-  $("#article_option_markdown").click(function() {
-    $("#wysiwig_content_container").hide();
-    $("#markdown_content_container").show();
-  });
-
-  $("#article_option_wysiwyg").click(function() {
-    $("#markdown_content_container").hide();
-    $("#wysiwig_content_container").show();
-  });
-
-  $("#cancel_writing").click(function() {
-    $(".article_writer").hide();
   });
 
   $("#cancel_edit").click(function() {
